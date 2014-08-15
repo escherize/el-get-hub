066b60a79c83d450131568cb00b1ce0c3bfbad44
Bryan Maass <bryan.maass@gmail.com>
2014-08-15 00:05:55 -0700

    Fix duplicate key + sort map

--- src/elgethub/schema.clj
+++ src/elgethub/schema.clj
@@ -6,38 +6,37 @@
 
 (def Recipe
   {:name s/Str
-   (s/optional-key :depends) s/Str
-   (s/optional-key :builtin) s/Str
-   (s/optional-key :pkgname) s/Str
-   (s/optional-key :type) s/Str
+   (s/optional-key :after) s/Str
+   (s/optional-key :autoloads) s/Str
+   (s/optional-key :before) s/Str
    (s/optional-key :branch) s/Str
-   (s/optional-key :url) s/Str
-   (s/optional-key :username) s/Str
-   (s/optional-key :pkgname) s/Str
    (s/optional-key :build) s/Str
    (s/optional-key :build/system-type) s/Str
-   (s/optional-key :load-path) s/Str
-   (s/optional-key :compile) s/Str
-   (s/optional-key :info) s/Str
-   (s/optional-key :load) s/Str
-   (s/optional-key :features) s/Str
-   (s/optional-key :autoloads) s/Str
-   (s/optional-key :library) s/Str
-   (s/optional-key :options) s/Str
-   (s/optional-key :module) s/Str
-   (s/optional-key :repo) s/Str
-   (s/optional-key :prepare) s/Str
-   (s/optional-key :before) s/Str
-   (s/optional-key :post-init) s/Str
-   (s/optional-key :after) s/Str
-   (s/optional-key :lazy) s/Str
-   (s/optional-key :localname) s/Str
-   (s/optional-key :website) s/Str
-   (s/optional-key :description) s/Str
-   (s/optional-key :checksum) s/Str
+   (s/optional-key :builtin) s/Str
    (s/optional-key :checkout) s/Str
+   (s/optional-key :checksum) s/Str
+   (s/optional-key :compile) s/Str
+   (s/optional-key :depends) s/Str
+   (s/optional-key :description) s/Str
+   (s/optional-key :features) s/Str
+   (s/optional-key :info) s/Str
+   (s/optional-key :lazy) s/Str
+   (s/optional-key :library) s/Str
+   (s/optional-key :load) s/Str
+   (s/optional-key :load-path) s/Str
+   (s/optional-key :localname) s/Str
+   (s/optional-key :module) s/Str
+   (s/optional-key :options) s/Str
+   (s/optional-key :pkgname) s/Str
+   (s/optional-key :post-init) s/Str
+   (s/optional-key :prepare) s/Str
+   (s/optional-key :repo) s/Str
    (s/optional-key :shallow) s/Str
-   (s/optional-key :submodule) s/Str})
+   (s/optional-key :submodule) s/Str
+   (s/optional-key :type) s/Str
+   (s/optional-key :url) s/Str
+   (s/optional-key :username) s/Str
+   (s/optional-key :website) s/Str})
 
 (def User
   {:name s/Str
