0903c18173b202e4072432569a437978ca05b0a3
Bryan Maass <bryan.maass@gmail.com>
2014-08-14 23:53:08 -0700

    Updated Recipe Schema to match the el-get docs

--- src/elgethub/schema.clj
+++ src/elgethub/schema.clj
@@ -4,15 +4,45 @@
 ;; docs at:
 ;; https://github.com/dimitri/el-get/blob/master/el-get.info for info
 
+(def Recipe
+  {:name s/Str
+   (s/optional-key :depends) s/Str
+   (s/optional-key :builtin) s/Str
+   (s/optional-key :pkgname) s/Str
+   (s/optional-key :type) s/Str
+   (s/optional-key :branch) s/Str
+   (s/optional-key :url) s/Str
+   (s/optional-key :username) s/Str
+   (s/optional-key :pkgname) s/Str
+   (s/optional-key :build) s/Str
+   (s/optional-key :build/system-type) s/Str
+   (s/optional-key :load-path) s/Str
+   (s/optional-key :compile) s/Str
+   (s/optional-key :info) s/Str
+   (s/optional-key :load) s/Str
+   (s/optional-key :features) s/Str
+   (s/optional-key :autoloads) s/Str
+   (s/optional-key :library) s/Str
+   (s/optional-key :options) s/Str
+   (s/optional-key :module) s/Str
+   (s/optional-key :repo) s/Str
+   (s/optional-key :prepare) s/Str
+   (s/optional-key :before) s/Str
+   (s/optional-key :post-init) s/Str
+   (s/optional-key :after) s/Str
+   (s/optional-key :lazy) s/Str
+   (s/optional-key :localname) s/Str
+   (s/optional-key :website) s/Str
+   (s/optional-key :description) s/Str
+   (s/optional-key :checksum) s/Str
+   (s/optional-key :checkout) s/Str
+   (s/optional-key :shallow) s/Str
+   (s/optional-key :submodule) s/Str})
+
 (def User
   {:name s/Str
-   :email s/Str})
-
-(def Recipe
-  :name s/Str
-  (s/optional-key :depends) [s/Str]
-  (s/optional-key :builtin)
-  {s/optional-key})
+   :email s/Str
+   :pass s/Str})
 
 (def Comment
   {:author User
