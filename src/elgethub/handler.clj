d8e6f213c518eea8e6bec3528d530c94fa44e6ce
Bryan Maass <bryan.maass@gmail.com>
2014-08-14 23:25:15 -0700

    Added Simple Sidebar @ "/" -> index.html

--- src/elgethub/handler.clj
+++ src/elgethub/handler.clj
@@ -1,10 +1,12 @@
 (ns elgethub.handler
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.util.response :as resp]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (resp/file-response "index.html" 
+                                  {:root "resources/public"}))
   (route/resources "/")
   (route/not-found "Not Found"))
 
